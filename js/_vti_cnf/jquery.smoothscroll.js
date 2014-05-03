c79d88f83b69f071923a4a44b5389a6dcbbc0143
Greg <greg_alt@ratajik.net>
2014-05-02 22:47:53 -0700

    first commit

---
+++ js/_vti_cnf/jquery.smoothscroll.js
@@ -0,0 +1,5 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|01 May 2014 02:41:12 -0000
+vti_extenderversion:SR|6.0.2.8161
+vti_backlinkinfo:VX|index.html
+vti_syncwith_www.afk.me\:80:TR|01 May 2014 02:41:12 -0000
